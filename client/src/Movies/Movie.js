8242b114c3278376d5550fd293c40ad0dc9a4592
CarnunMP <carnun@hotmail.co.uk>
2019-09-16 16:24:14 +0100

    part 2 done

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,10 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
- 
+  const [movie, setMovie] = useState(null);
   useEffect(() => {
-    const id = props.path;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -18,7 +17,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[movie]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
@@ -30,7 +29,7 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  const { title, director, metascore, stars } = movie;  
   return (
     <div className="save-wrapper">
       <div className="movie-card">
