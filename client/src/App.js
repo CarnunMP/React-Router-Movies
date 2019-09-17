8a676d88212ea8cd006862f9d6a31ea287b130ef
CarnunMP <carnun@hotmail.co.uk>
2019-09-17 15:20:38 +0100

    save working

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SavedList from './Movies/SavedList';
 import { Route } from "react-router-dom";
 import MovieList from "./Movies/MovieList";
@@ -7,15 +7,30 @@ import Movie from "./Movies/Movie";
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+  const addToSavedList = (movie) => { // Hmm... seems to be working. But is quite convoluted!
+    let movieIsInList = false;
+
+    if (savedList.length >= 1) {
+      savedList.forEach(movieInList => {
+        if (movie.title === movieInList.title) {
+          movieIsInList = true;
+          return;
+        }
+      });
+    }
+
+    if (!movieIsInList) {
+      setSavedList(savedList.concat([movie]))
+    }
+  }
 
   return (
     <div>
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList}/>
-      <Route path="/movies/:id" component={Movie}/>
+      <Route path="/movies/:id" render={props => {
+        return <Movie {...props} addToSavedList={addToSavedList}/>
+      }}/>
     </div>
   );
 };
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -21,10 +21,10 @@ const Movie = (props) => {
   },[movie]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -35,7 +35,7 @@ const Movie = (props) => {
       <div className="movie-card">
         <MovieCard {...movie}/>
       </div>
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={saveMovie}>Save</div>
     </div>
   );
 }
